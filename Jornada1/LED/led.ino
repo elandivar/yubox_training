24ecdf6e8fba03017f290fad495097d9b3979abe
Edgar Landivar <elandivar@github.com>
2020-07-16 16:16:59 -0500

    adicionando ejemplos para Jornada1

---
+++ Jornada1/LED/led.ino
@@ -0,0 +1,19 @@
+// Yubox Companion
+// Prueba LEDs on-board
+// Created by: Edgar Landivar
+// Usted es libre de copiar y distribuir
+// www.yubox.com
+
+#define LED1 4
+
+void setup() {
+  pinMode(LED, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED, HIGH);   
+  delay(1000);     
+  digitalWrite(LED, LOW);   
+  delay(1000);            
+}
