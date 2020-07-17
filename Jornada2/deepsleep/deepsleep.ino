83744694a97a97b3a1ed949193ce0585a3f9fbd4
Edgar Landivar <elandivar@github.com>
2020-07-16 19:56:50 -0500

    ejemplo deep sleep

---
+++ Jornada2/deepsleep/deepsleep.ino
@@ -0,0 +1,13 @@
+RTC_DATA_ATTR int bootCount = 0;
+int GREEN_LED_PIN = 4;
+void setup(){
+    pinMode(GREEN_LED_PIN,OUTPUT);
+    delay(500);
+    digitalWrite(GREEN_LED_PIN,HIGH);
+    delay(3000);
+    digitalWrite(GREEN_LED_PIN,LOW);
+    esp_sleep_enable_timer_wakeup(6000000);
+    esp_deep_sleep_start();
+}
+void loop(){
+}
