0c167bb4fc490542ee6c423ed47fa4832bf04502
Edgar Landivar <elandivar@github.com>
2021-03-08 13:28:40 -0500

    nuevo ejercicio Touch

---
+++ Essentials/Jornada3/Touch/Touch.ino
@@ -0,0 +1,25 @@
+#include "Nextion.h"
+
+// Page 0
+NexText t0        = NexText(0, 3, "t0");
+NexDSButton b0    = NexDSButton(0, 2, "b0");
+
+NexTouch *nex_listen_list[] = {&b0, NULL};
+
+void setup() {
+   Serial.begin(115200);
+   Serial2.begin(9600, SERIAL_8N1, 25, 32); 
+   nexInit();
+
+   //Botones
+   b0.attachPush(boton_presionado, &0);
+   t0.setText("Hola, presiona el boton");
+}
+
+void loop() {
+      nexLoop(nex_listen_list);
+}
+
+void boton_presionado(void *ptr) {
+    t0.setText("Bien!!!!");
+}
