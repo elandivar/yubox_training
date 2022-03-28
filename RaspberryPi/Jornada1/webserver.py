94375c1029193c5812d3716750036ae858dafbfc
Edgar <elandivar@yubox.com>
2022-03-28 21:26:03 +0100

    adicionando archivos de la jornada 1

---
+++ RaspberryPi/Jornada1/webserver.py
@@ -0,0 +1,25 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "172.16.237.185"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><body><p>Hola Mundo</p></body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
