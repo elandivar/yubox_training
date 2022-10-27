f2d1a502756d310212368cef948dc7ac45a4e6f4
Edgar Landivar <elandivar@yubox.com>
2022-10-27 14:16:54 -0500

    Create i2c_scanner_yubox_one.ino

---
+++ Essentials/Jornada2/I2C/i2c_scanner/i2c_scanner_yubox_one.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+
+void setup() {
+
+  delay(6000); // Usar en caso de problemas
+  Serial.begin (9600);
+  Serial.println ("I2C scanner. Escaneando dispositivos I2C...");
+  byte count = 0;
+  Wire.setPins(40,41);
+  Wire.begin();
+  //Wire.begin(32,25);
+
+  for (byte i = 8; i < 120; i++)
+  {
+    Wire.beginTransmission (i);
+    if (Wire.endTransmission () == 0)
+      {
+      Serial.print ("Found address: ");
+      Serial.print (i, DEC);
+      Serial.print (" (0x");
+      Serial.print (i, HEX);
+      Serial.println (")");
+      count++;
+      delay (1);  // maybe unneeded?
+      } // end of good response
+  } // end of for loop
+  Serial.println ("Done.");
+  Serial.print ("Found ");
+  Serial.print (count, DEC);
+  Serial.println (" device(s).");
+}  // end of setup
+
+void loop() {}
